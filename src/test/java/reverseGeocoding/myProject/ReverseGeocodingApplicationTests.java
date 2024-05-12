230c7a00a8abf4859b0f8f12b92d817bf2772c30
Taewan Kim <paladyn12@naver.com>
2024-05-13 02:20:24 +0900

    Initial commit

---
+++ src/test/java/reverseGeocoding/myProject/ReverseGeocodingApplicationTests.java
@@ -0,0 +1,13 @@
+package reverseGeocoding.myProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReverseGeocodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
