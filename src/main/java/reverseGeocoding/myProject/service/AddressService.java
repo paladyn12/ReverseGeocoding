a2f4189235c55971db638923bb310ab53be6650d
Taewan Kim <paladyn12@naver.com>
2024-05-17 02:54:35 +0900

    Reverse Geocoding

--- src/main/java/reverseGeocoding/myProject/service/AddressService.java
+++ src/main/java/reverseGeocoding/myProject/service/AddressService.java
@@ -84,6 +84,7 @@ public class AddressService {
                     addr.append(name).append(" ");
                 }
             }
+
             // 추출한 두 주소 합치기
             String roadAddr = addr + detailAddr;
             log.info("roadAddr={}", roadAddr);
